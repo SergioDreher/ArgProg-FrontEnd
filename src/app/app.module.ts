cc9a98a726f962607fc00c07add7d9915f4177ae
SergioDreher <sergio.leo.dreher@gmail.com>
2022-07-31 22:12:14 -0300

    FRONT END PARA ENTREGAR

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { NewExperienciaComponent } from './components/experiencia/newexperiencia.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectoComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    NewExperienciaComponent,
+    EditeducacionComponent
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/model/nuevo-usuario.ts
@@ -0,0 +1,7 @@
+export class NuevoUsuario {
+    nombre!: string;
+    nombreUusario!: string;
+    email!: string;
+    password!: string;
+    authorities!:string[];
+}
